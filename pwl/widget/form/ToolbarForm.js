9c2850e73b9061cd28a8e3d0acfd503c711e5440
Peter Ivan <ra-ven@ra-ven.net>
2011-06-16 11:53:45 +0200

    Create form with accept widget and optional toolbar: ToolbarForm.

---
+++ pwl/widget/form/ToolbarForm.js
@@ -0,0 +1,70 @@
+dojo.provide('pwl.widget.form.ToolbarForm');
+
+/******************************************************************************/
+/******************************************************************************/
+
+dojo.require('dijit.form.Form');
+
+dojo.require('dijit.Toolbar');
+
+dojo.require('pwl.widget._Acceptable');
+
+/******************************************************************************/
+
+dojo.declare(
+	'pwl.widget.form.ToolbarForm',
+	[dijit.form.Form, pwl.widget._Acceptable],
+{
+	baseClass: 'pwlWidgetFormToolbarForm',
+
+	templateString: dojo.cache('pwl.widget.form', 'templates/ToolbarForm.html'),
+
+	toolbar_position: 'top',
+
+/******************************************************************************/
+/** public **/
+/******************************************************************************/
+
+/******************************************************************************/
+/** Startup, Teardown *********************************************************/
+
+	startup: function ()
+	{
+		this.inherited(arguments);
+
+		this.getChildren().forEach( function ( i_child )
+		{
+			if ( i_child.declaredClass && i_child.declaredClass == 'dijit.Toolbar' )
+			{
+				i_child.placeAt(this.n_toolbar);
+			}
+		}, this);
+
+		this.set('toolbar_position', this.toolbar_position);
+	},
+
+/******************************************************************************/
+/** protected **/
+/******************************************************************************/
+
+/******************************************************************************/
+/** Attr handlers *************************************************************/
+
+	_setToolbar_positionAttr: function ( i_position )
+	{
+		dojo.removeClass(this.n_toolbar, 'top');
+		dojo.removeClass(this.n_toolbar, 'bottom');
+
+		switch ( i_position )
+		{
+			case 'bottom':
+				dojo.place(this.n_toolbar, this.containerNode, 'after');
+				dojo.addClass(this.n_toolbar, 'bottom');
+				break;
+
+			default: // top
+				dojo.place(this.n_toolbar, this.containerNode, 'before');
+				dojo.addClass(this.n_toolbar, 'top');
+		}
+	}
+});
